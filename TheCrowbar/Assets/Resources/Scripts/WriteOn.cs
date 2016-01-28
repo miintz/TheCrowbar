0633477635874b53ac9009a4099d0747bd9e03a9
miintz <vanheesmaarten@hotmail.com>
2016-01-28 21:27:55 +0100

    maarten: first version of writeon script

---
+++ TheCrowbar/Assets/Resources/Scripts/WriteOn.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+public class WriteOn : MonoBehaviour {
+
+    public int WriteOnRate; //frames?
+    public Text Target; //text script target
+
+    private String[] Words;
+    private float WordTimer;
+    private int CurrentIndex;
+
+	void Start () {
+	    //hide text
+        String TargetText = Target.text;
+        Target.text = "";
+
+        Words = TargetText.Split(' '); //dit mag blijkbaar tegenwoordig
+        WordTimer = 0.0f;
+        
+        CurrentIndex = 0;
+	}
+	
+	void Update () {
+        WordTimer += Time.deltaTime * 1000;
+
+        if (WordTimer >= WriteOnRate && CurrentIndex < Words.Length)
+        {
+            Target.text = "";
+
+            for (int i = 0; i < CurrentIndex; i++)
+            {                
+                if (i != 0)
+                    Target.text += " " + Words[i];
+                else
+                    Target.text += Words[i];
+            }
+
+            CurrentIndex++;
+            WordTimer = 0.0f;
+        }    
+    }
+}
