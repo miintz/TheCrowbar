c5d67b9d7dd1e3a186aa48d44e7e013b0f11e687
miintz <vanheesmaarten@hotmail.com>
2016-02-01 23:02:21 +0100

    maarten: writeon script voor tab

--- TheCrowbar/Assets/Resources/Scripts/WriteOn.cs
+++ TheCrowbar/Assets/Resources/Scripts/WriteOn.cs
@@ -2,35 +2,57 @@
 using System.Collections;
 using UnityEngine.UI;
 using System;
-public class WriteOn : MonoBehaviour {
+using UnityEngine.EventSystems;
 
-    public int WriteOnRate; //frames?
+public class WriteOn : MonoBehaviour
+{
+    public int WriteOnRate = 150; //woord per zoveel ms
     public Text Target; //text script target
+    public String PauseChar;
+    public bool Interactable;
+    public bool NoTouch = true;
+    public int Delay;
 
     private String[] Words;
     private float WordTimer;
     private int CurrentIndex;
+    private bool Delaying = false;
+    private bool Delayed = false;
 
-	void Start () {
-	    //hide text
+    void Start()
+    {
+        //hide text
         String TargetText = Target.text;
         Target.text = "";
 
         Words = TargetText.Split(' '); //dit mag blijkbaar tegenwoordig
         WordTimer = 0.0f;
-        
+
         CurrentIndex = 0;
-	}
-	
-	void Update () {
+
+        if (Delay > 0)
+        {
+            Delaying = true;
+            Delayed = Delaying;
+        }
+    }
+
+    public void Update()
+    {
         WordTimer += Time.deltaTime * 1000;
 
-        if (WordTimer >= WriteOnRate && CurrentIndex < Words.Length)
+        //eerst moet de delay voorbij zijn
+        if (Delaying && WordTimer >= Delay)
+        {
+            Delaying = false;
+            WordTimer = 0.0f;
+        }
+
+        if (WordTimer >= WriteOnRate && CurrentIndex < Words.Length && !Delaying)
         {
             Target.text = "";
-
-            for (int i = 0; i < CurrentIndex; i++)
-            {                
+            for (int i = 0; i < CurrentIndex + 1; i++)
+            {
                 if (i != 0)
                     Target.text += " " + Words[i];
                 else
@@ -39,6 +61,57 @@ public class WriteOn : MonoBehaviour {
 
             CurrentIndex++;
             WordTimer = 0.0f;
-        }    
+        }
+
+        if (Interactable)
+        {
+            int fingerCount = 0;
+            foreach (Touch touch in Input.touches)
+            {
+                if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
+                    fingerCount++;
+            }
+
+            if(NoTouch && Input.GetMouseButtonDown(0))
+            {
+                Interact();
+            }
+            else if (fingerCount > 0)
+            {
+                Interact();    
+            }
+        }
+    }
+
+    public void Reset()
+    {
+        if (Delayed)
+            Delaying = Delayed;
+
+        String TargetText = Target.text;
+        Target.text = "";
+
+        Words = TargetText.Split(' '); //dit mag blijkbaar tegenwoordig
+        WordTimer = 0.0f;
+
+        CurrentIndex = 0;
+    }
+
+    private void Interact()
+    {        
+        //omzeil eventsystem    
+        //WriteOn[] o = (WriteOn[])Resources.FindObjectsOfTypeAll(typeof(WriteOn));       
+        
+        //foreach (WriteOn writer in o)
+        //{
+        //    if (writer.Interactable)
+        //    {
+        //        Vector3 p = writer.gameObject.transform.position;
+        //        Vector3 m = Input.mousePosition;
+
+        //        float distance = Vector3.Distance(p, m);
+        //        Debug.Log(writer.gameObject.name + " " + distance);
+        //    }
+        //}
     }
 }
---
+++ TheCrowbar/Assets/Resources/Scripts/WriteOnManager.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class WriteOnManager : MonoBehaviour {
+
+    private Dictionary<int, List<string>> Answers;
+
+    public Text TheCrow;
+    public Text QuestionOne;
+    public Text QuestionTwo;
+
+    private int CrowIndex;
+    private int QuestionIndex;
+
+    [TextArea(3, 10)]
+    public string TheCrowText;
+
+    [TextArea(3, 10)]
+    public string QuestionsForOne;
+
+    [TextArea(3, 10)]
+    public string QuestionsForTwo;
+
+    public char DelimiterCharacter;
+
+	// Use this for initialization
+	void Start () {
+        Answers = new Dictionary<int, List<string>>();
+
+        List<string> lines = new List<string>();
+        lines.AddRange(QuestionsForOne.Split(DelimiterCharacter));
+
+        Answers.Add(0, lines);
+
+        lines = new List<string>();
+        lines.AddRange(QuestionsForTwo.Split(DelimiterCharacter));
+
+        Answers.Add(1, lines);
+
+        lines = new List<string>();
+        lines.AddRange(TheCrowText.Split(DelimiterCharacter));
+
+        Answers.Add(2, lines);
+
+        setAnswers();
+        QuestionIndex++;
+        CrowIndex++;
+	}
+
+    void setAnswers(bool crowOnly = false)
+    {
+        if (!crowOnly)
+        {
+            List<string> firstlines = new List<string>();
+            Answers.TryGetValue(0, out firstlines);
+            QuestionOne.text = firstlines[QuestionIndex];
+
+            List<string> secondlines = new List<string>();
+            Answers.TryGetValue(1, out secondlines);
+            QuestionTwo.text = secondlines[QuestionIndex];
+        }
+       
+        List<string> crowlines = new List<string>();
+        Answers.TryGetValue(2, out crowlines);
+        TheCrow.text = crowlines[CrowIndex];
+       
+    }
+
+    void resetTextEntities()
+    {
+        QuestionOne.GetComponent<WriteOn>().Reset();
+        QuestionTwo.GetComponent<WriteOn>().Reset();
+        TheCrow.GetComponent<WriteOn>().Reset();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        //for (int i = 0; i < Answers.Count; i++)
+        //{
+        //    List<string> list = new List<string>();
+        //    Answers.TryGetValue(i, out list);
+
+        //    foreach (string answer in list)
+        //    {
+        //        //Debug.Log(answer + " " + i);
+        //    }
+        //}
+	}
+
+    public void NextAnswer()
+    {
+        setAnswers();
+        resetTextEntities();
+    }
+}
